d95eec7f72069145aee27e9a3979e17c155480fb
mampfi <burgham@esrf.fr>
2021-04-09 00:06:16 +0200

    still working on addint roi map sum function

--- gui/id13_proc_gui.py
+++ gui/id13_proc_gui.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('../xs_proc')
 from h5_data_search import *
+from xs_data_proc import *
+from proc_data_ana import *       
 import os
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
@@ -13,7 +15,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import file_tree as ft
 
-class input_window(QWidget):
+class input_window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.input_UI()
@@ -25,26 +27,40 @@ class input_window(QWidget):
         path = QWidget(self)
         path.setGeometry(20,20,180,20)
         path_name = QLabel('path',path)
-        self.path_input = QLineEdit('',path)
+        #self.path_input = QLineEdit('',path)
+        self.path_input = QLineEdit('/data/id13/inhouse12/DATAPOLICY_I12_1/eh2/inhouse/blc12867/id13',path)
         self.path_input.move(60,0)
         self.path_input.textEdited.connect(self.path_set)
 
         prop_id = QWidget(self) 
         prop_id.setGeometry(20,50,180,20)
         id_name = QLabel('proposal',prop_id)
-        self.id_input   = QLineEdit('',prop_id)
+        #self.id_input   = QLineEdit('',prop_id)
+        self.id_input   = QLineEdit('blc12867',prop_id)
         self.id_input.move(60,0)
         self.id_input.textEdited.connect(self.id_set)
        
         self.tree = QTreeWidget(self)
-        self.tree.setGeometry(220,20,360,320)
+        self.tree.setGeometry(220,20,360,340)
         header = QTreeWidgetItem(['File'])
         self.tree.setHeaderItem(header)
+        
+        self.msgbox = QListWidget(self)
+        self.msgbox.insertItem(0,'scan info:\n')
+        self.msgbox.setGeometry(600,20,360,160)
+        
+        self.proc_msg = QTextBrowser(self)
+        self.proc_msg.setGeometry(600,200,360,160)
         
         info_enter = QPushButton("enter",self)
         info_enter.setGeometry(20,380,60,20)
         info_enter.clicked.connect(self.load_id13_h5)
         
+        proc_widget = QWidget(self)
+        proc_widget.setGeometry(1000,20,260,320)
+        roi_sum_proc = QPushButton("roi sum",proc_widget)
+        roi_sum_proc.setGeometry(10,10,60,20)
+        roi_sum_proc.clicked.connect(self.roi_sum_proc)
         self.show()
     
     def path_set(self):
@@ -68,7 +84,7 @@ class input_window(QWidget):
     
     def add_file(self):
         try:
-            print(1)
+            #print(1)
             path = os.path.join(self.proposal_path,
                    '{}_id13.h5'.format(self.proposal_id))
             with h5py.File(path,'r') as f:
@@ -96,7 +112,111 @@ class input_window(QWidget):
     def onItemClicked(self,item):
         self.sample_keyword = item.text(0)
         self.scan_obj.keyword_search(self.sample_keyword)
+        self.msgbox.clear()
+        self.msgbox.insertItem(0,'scan info:')
+        try:
+            for _,__ in enumerate(self.scan_obj.data_info.keys()):
+                msg = str(__) + ':  ' + \
+                      str(self.scan_obj.data_info[__])
+                self.msgbox.insertItem((_+1),msg)
+            self.total_pttns,self.scan_shape,self.idx_list = \
+                    scan_info(self.scan_obj)
+            self.h5_list,self.path_idx,self.pttn_idx = \
+                    scan_h5_data_info(self.scan_obj,
+                                    self.scan_shape,self.idx_list)
+        except Exception as e:
+            self.proc_msg.setText(str(e))
+            pass
+
+    def roi_sum_proc(self):
+        try:
+            self.roi_sum_win = roi_sum(self.h5_list,
+                        self.path_idx,self.pttn_idx,
+                        self.scan_shape)
+            self.roi_sum_win.show()
+        except Exception as e:
+            self.proc_msg.setText(str(e))
+            pass
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent,
+    it will appear as a free-floating window.
+    """
+
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window % d" % 1)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+            
+class roi_sum(QWidget):
+    def __init__(self,h5_list,path_idx,pttn_idx,scan_shape):
+        super().__init__()
+        self.h5_list  = h5_list
+        self.path_idx = path_idx
+        self.pttn_idx = pttn_idx
+        self.scan_shape = scan_shape
+        self.roi_sum_UI()
+        self.show()
         
+    def roi_sum_UI(self):
+        self.setGeometry(500,40,900,480)
+        self.setWindowTitle('roi_sum')
+        
+        up_corner = QWidget(self)
+        up_corner.setGeometry(20,20,180,20)
+        up_corner_label = QLabel('up corner',up_corner)
+        up_corner_label.setGeometry(0,0,60,20)
+        up_corner_input = QLineEdit('0,0',up_corner)
+        up_corner_input.setGeometry(80,0,60,20)
+        up_corner_input.textChanged[str].connect(self.ul_corner_set)
+        
+        dr_corner = QWidget(self)
+        dr_corner.setGeometry(20,60,180,20)
+        dr_corner_label = QLabel('down corner',dr_corner)
+        dr_corner_label.setGeometry(0,0,60,20)
+        dr_corner_input = QLineEdit('-1,-1',dr_corner)
+        dr_corner_input.setGeometry(80,0,60,20)
+        dr_corner_input.textChanged[str].connect(self.dr_corner_set)
+        
+        proc_button = QPushButton('process',self)
+        proc_button.setGeometry(20,240,60,20)
+        proc_button.clicked.connect(self.roi_sum_process)
+        self.show()
+         
+    def ul_corner_set(self,text):
+        try:
+            t = text.split(',')
+            self.ul =  (int(t[0]),int(t[1]))
+        except:
+            pass
+    
+    def dr_corner_set(self,text):
+        try:
+            t = text.split(',')
+            self.dr =  (int(t[0]),int(t[1]))
+        except:
+            pass
+    
+    def roi_sum_process(self):
+        try:
+            res = parallel_func(scan_pttn_roi_sum,12,
+                                np.arange(len(self.path_idx.flatten())),
+                                h5_list  = self.h5_list,
+                                path_idx = self.path_idx.flatten(),
+                                pttn_idx = self.pttn_idx.flatten(),
+                                data_path = 'entry_0000/measurement/data',
+                                #left_top  = (0,0),
+                                #right_bottom = (-1,-1),
+                                )
+            self.roi_map = np.array(res).reshape(self.scan_shape)     
+            print(self.roi_map.shape)
+        except Exception as e:
+            print(str(e))
+            pass
+                             
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = input_window()
